ed3825f9825480d8b1da320bfc942c4e0aac0922
manarshawkey <manarshawkey97@gmail.com>
2022-03-20 17:31:49 +0200

    defingin example purpose

--- app/src/main/java/com/example/android/eat/HandlerCallBackActivity.java
+++ app/src/main/java/com/example/android/eat/HandlerCallBackActivity.java
@@ -1,7 +1,15 @@
 package com.example.android.eat;
 
 /*
- *
+ * Implementing the Handler.Callback interface is an
+ * alternative way to extend the handler class.
+ * The Callback interface defines a handleMessage method
+ * which acts as a preprocessor of messages passed to the
+ * handler. It has a boolean return value, where true
+ * meaning that no further processing of the message
+ * is required and false indicating that the message
+ * needs further processing in which case the message is
+ * passed on to the Handler.handleMessage method.
  */
 import androidx.appcompat.app.AppCompatActivity;
 
