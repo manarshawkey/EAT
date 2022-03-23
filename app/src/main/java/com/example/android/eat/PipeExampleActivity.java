1f0b1da86abde9fd7dd9a6788810c9cd50381402
manarshawkey <manarshawkey97@gmail.com>
2022-03-23 16:57:54 +0200

    defining example purpose

--- app/src/main/java/com/example/android/eat/PipeExampleActivity.java
+++ app/src/main/java/com/example/android/eat/PipeExampleActivity.java
@@ -1,5 +1,14 @@
 package com.example.android.eat;
 
+
+/*
+ * This example illustrates how pipes can process
+ * text that a user enters in an EditText.
+ * To keep the UI thread responsive, each character
+ * entered by the user is passed to a worker thread,
+ * which presumably handles some time consuming processing.
+ * Here, we're only printing the char to the logs.
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +19,6 @@ public class PipeExampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pipe_example);
+
     }
 }
