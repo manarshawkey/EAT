9e14a78ea0f97f0bc9cef0b5f587d7bf6b7dd686
manarshawkey <manarshawkey97@gmail.com>
2022-03-23 15:58:44 +0200

    adding pipe example

---
+++ app/src/main/java/com/example/android/eat/PipeExampleActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.eat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PipeExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pipe_example);
+    }
+}
