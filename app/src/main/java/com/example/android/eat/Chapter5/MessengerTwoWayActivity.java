198eabec73698b75d0b745e24fa3899fad1a5425
manarshawkey <manarshawkey97@gmail.com>
2022-04-24 23:25:08 +0200

    adding a dialog box explaining two-way communication example

--- app/src/main/java/com/example/android/eat/Chapter5/MessengerTwoWayActivity.java
+++ app/src/main/java/com/example/android/eat/Chapter5/MessengerTwoWayActivity.java
@@ -16,8 +16,11 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.android.eat.R;
+import com.example.android.eat.Utils;
+
 public class MessengerTwoWayActivity extends AppCompatActivity {
 
     private final String LOG_TAG = MessengerTwoWayActivity.class.getSimpleName();
@@ -43,6 +46,13 @@ public class MessengerTwoWayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messenger_two_way);
         setUpSendMessageButton();
+        Utils.displayDialog("This example illustrates two-way communication " +
+                "between an activity and a service executing in different " +
+                "processes. The activity sends a message with a replyTo argument " +
+                ", to a remote service. The remote service receives the Message and send " +
+                "a new message back to the activity. On pressing the button " +
+                "the what value of the message sent back from the service to the activity " +
+                "should be displayed. That is the value '3'", "Two-way Communication", this);
     }
 
     private void setUpSendMessageButton() {
@@ -54,6 +64,8 @@ public class MessengerTwoWayActivity extends AppCompatActivity {
                     @Override
                     public void handleMessage(@NonNull Message msg) {
                         Log.d(LOG_TAG, "Message sent back - Msg.what = " + msg.what);
+                        Toast.makeText(MessengerTwoWayActivity.this,
+                                "Message sent back from service to activity - Msg.what = " + msg.what, Toast.LENGTH_SHORT).show();
                     }
                 });
                 try {
